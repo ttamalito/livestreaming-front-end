6357f78bf26cce599c1ca2ea7f1931b049884737
Jose Gonzalez <coding.tamalito@gmail.com>
2024-02-15 13:08:05 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
 
 function App() {
+    useEffect(() => {
+        fetch(`https://localhost:8443/`,
+            {method: 'GET'}).then(
+            res => {
+                console.log(res)
+                res.text().then(data => {console.log(data)})
+            }
+        )
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
